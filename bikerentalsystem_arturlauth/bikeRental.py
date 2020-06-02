f23d7f783dc5cd540f9cc21839992e1bf1f5012c
arturlauth-dev <arturlauthmain@gmail.com>
2020-06-02 15:17:44 -0300

    adicionado main para rodar o programa

--- bikerentalsystem_arturlauth/bikeRental.py
+++ bikerentalsystem_arturlauth/bikeRental.py
@@ -105,20 +105,12 @@ class Customer():
         self.bikes = 0
         self.bill = 0
 
-    def rentBike(self):
+    def requestBike(self):
 
-        self.bikes = input("Number of bikes:")
-        self.rentalBasis = input("Type of rent:")
+        bikes = input("Number of bikes: ")
+        self.bikes = int(bikes)
 
-        if rentalBasis == 1:
-            self.rentalTime = BikeRental.rentBikeOnHourlyBasis(bikes)
-
-        elif rentalBasis == 2:
-            self.rentalTime = BikeRental.rentBikeOnDailyBasis(bikes)
-
-        else:
-            self.rentalTime = BikeRental.rentBikeOnDailyBasis(bikes)
-
+        return self.bikes
 
     def returnBike(self):
 
@@ -132,55 +124,3 @@ class Customer():
             return 0, 0, 0
 
         return self.rentalTime, self.rentalBasis, self.bikes
-
-
-
-if __name__ == '__main__':
-    shop = BikeRental(50)
-    customer1 = Customer()
-    customer2 = Customer()
-    customer3 = Customer()
-    customer4 = Customer()
-    customer5 = Customer()
-    customer6 = Customer()
-
-    # create valid rentalBasis for each customer
-    customer1.rentalBasis = 1  # hourly
-    customer2.rentalBasis = 1  # hourly
-    customer3.rentalBasis = 2  # daily
-    customer4.rentalBasis = 2  # daily
-    customer5.rentalBasis = 3  # weekly
-    customer6.rentalBasis = 3  # weekly
-
-    # create valid bikes for each customer
-    customer1.bikes = 1
-    customer2.bikes = 5  # eligible for family discount 30%
-    customer3.bikes = 2
-    customer4.bikes = 8
-    customer5.bikes = 15
-    customer6.bikes = 30
-
-    # create past valid rental times for each customer
-
-    customer1.rentalTime = datetime.now() + timedelta(hours=-4)
-    customer2.rentalTime = datetime.now() + timedelta(hours=-23)
-    customer3.rentalTime = datetime.now() + timedelta(days=-4)
-    customer4.rentalTime = datetime.now() + timedelta(days=-13)
-    customer5.rentalTime = datetime.now() + timedelta(weeks=-6)
-    customer6.rentalTime = datetime.now() + timedelta(weeks=-12)
-
-    # make all customers return their bikes
-    request1 = customer1.returnBike()
-    request2 = customer2.returnBike()
-    request3 = customer3.returnBike()
-    request4 = customer4.returnBike()
-    request5 = customer5.returnBike()
-    request6 = customer6.returnBike()
-
-    # check if all of them get correct bill
-    shop.returnBike(request1)# 20)
-    shop.returnBike(request2)# 402.5)
-    shop.returnBike(request3)# 160)
-    shop.returnBike(request4)# 2080)
-    shop.returnBike(request5)# 5400)
-    shop.returnBike(request6)# 21600)
